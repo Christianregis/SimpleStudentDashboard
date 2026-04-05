cbab10e91648807d8f63beffa21f4ab60402223f
Chritianregis <christiancheukap@gmailcom>
2026-04-05 18:29:22 +0100

    Introduction des commentaires (Simple Documentation)

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -13,15 +13,21 @@ import Slidebar from "@/components/slidebar.vue";
 import Statistics from "@/components/Statistics.vue";
 import { ref } from "vue";
 
+// Variable representant la moyenne generale de la classe (Elle est par defaut 0)
 let averageCount = ref(0);
+
+// Recuperationde la moyenne generale de la classe grace au emits passee du composant AverageComponent
 let getGeneralAverage = (generalAverage)=>{
   averageCount.value = generalAverage;
 }
 
+// Valeur par defaut au cas ou nous ne recevons pas les donnees du meilleur eleve
 let studentBest = ref({
   name: "NAN",
   average: 0
 })
+
+// recuperation du meilleur eleves grace au emits passee dans le composants AverageComponent.vue
 const getBestStudent = (best_student)=>{
   studentBest.value = best_student;
 }
